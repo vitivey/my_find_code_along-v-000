a86dda0c23e030383de538d42166753f20697282
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-08-08 16:49:34 +0000

    automatically backed up by learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,8 @@ require 'pry'
 def my_find(collection)
   i=0
   while i < collection.size
-    return collection[i] if yield(collection[i])
+    return collection[i] if yield(collection[i]) == true
+    binding.pry
     i+=1
   end
 end
